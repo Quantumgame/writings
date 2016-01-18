07599806fb0565c85d44d96ebd697cc1ca4d7785
Mike Schachter <mike.schachter@gmail.com>
2016-01-17 18:50:33 -0800

    working on crcns figure

--- DecodingRhythms/results_crcns/figure.py
+++ DecodingRhythms/results_crcns/figure.py
@@ -10,50 +10,48 @@ import matplotlib.pyplot as plt
 from DecodingRhythms.utils import COLOR_YELLOW_SPIKE, get_this_dir, COLOR_BLUE_LFP
 from lasp.sound import log_transform
 from utils import set_font
+from zeebeez.aggregators.pairwise_cf import AggregatePairwiseCF
 from zeebeez.transforms.pairwise_cf import PairwiseCFTransform
 
 
 def plot_psds(psd_file, data_dir='/auto/tdrive/mschachter/data'):
 
     # read PairwiseCF file
-    pcf_file = os.path.join(data_dir, 'GreBlu9508M', 'transforms', 'PairwiseCF_GreBlu9508M_Site4_Call1_L_raw.h5')
-    pcf = PairwiseCFTransform.load(pcf_file)
+    pcf_file = os.path.join(data_dir, 'aggregate', 'pairwise_cf.h5')
+    pcf = AggregatePairwiseCF.load(pcf_file)
+    pcf.zscore_within_site()
 
     # get the LFP PSDs
-    i = (pcf.df.decomp == 'locked') & (pcf.df.electrode1 == pcf.df.electrode2)
-    indices = pcf.df['index'][i].values
-    lfp_psds = pcf.psds[indices, :]
+    indices = pcf.df['lfp_index'].unique()
+    lfp_psds = pcf.lfp_psds[indices, :]
     psum = lfp_psds.sum(axis=1)
     nz = psum > 0
     lfp_psds = lfp_psds[nz, :]
     log_transform(lfp_psds)
     lfp_psd_mean = lfp_psds.mean(axis=0)
     lfp_psd_std = lfp_psds.std(axis=0, ddof=1)
+    nsamps_lfp = lfp_psds.shape[0]
 
     # get the PSTH PSDs
-    i = (pcf.df.decomp == 'spike_psd') & (pcf.df.electrode1 == pcf.df.electrode2)
-    indices = pcf.df['index'][i].values
-    spike_psds = pcf.spike_psd[indices, :]
+    indices = pcf.df['spike_index'].unique()
+    spike_psds = pcf.spike_psds[indices, :]
     psum = spike_psds.sum(axis=1)
     nz = psum > 0
     spike_psds = spike_psds[nz, :]
     log_transform(spike_psds)
     spike_psd_mean = spike_psds.mean(axis=0)
     spike_psd_std = spike_psds.std(axis=0, ddof=1)
-
-    # get the spike rates
-    spike_rates = pcf.spike_rate[indices, :]
-    spike_rates = spike_rates[nz, :]
+    nsamps_spike = spike_psds.shape[0]
 
     # get the spike field coherence
-    i = (pcf.df.decomp == 'spike_field_coherence') & (pcf.df.electrode1 == pcf.df.electrode2)
-    indices = pcf.df['index'][i].values
-    spike_field_coherence = pcf.spike_field_coherence[indices, :]
-    psum = spike_field_coherence.sum(axis=1)
+    indices = pcf.df['spike_index'].unique()
+    spike_field_coherences = pcf.spike_field_coherences[indices, :]
+    psum = spike_field_coherences.sum(axis=1)
     nz = psum > 0
-    spike_field_coherence = spike_field_coherence[nz, :]
-    sfc_mean = spike_field_coherence.mean(axis=0)
-    sfc_std = spike_field_coherence.std(axis=0, ddof=1)
+    spike_field_coherences = spike_field_coherences[nz, :]
+    log_transform(spike_field_coherences)
+    spike_field_coherences_mean = spike_field_coherences.mean(axis=0)
+    spike_field_coherences_std = spike_field_coherences.std(axis=0, ddof=1)
 
     # read CRCNS file
     cell_data = dict()
@@ -73,49 +71,23 @@ def plot_psds(psd_file, data_dir='/auto/tdrive/mschachter/data'):
     # compute low gamma center frequencies
     fi_gamma = (freqs > 0) & (freqs < 100) # 16-66Hz
 
-    # draw the spike rate vs each frequency band power
-    """
-    nrows = 2
-    ncols = 6
-    fig = plt.figure()
-    fig.subplots_adjust(wspace=0.40, hspace=0.40)
-    for k,f in enumerate(pcf.freqs):
-        ax = plt.subplot(nrows, ncols, k+1)
-        x = spike_psds[:, k]
-        r = spike_rates[:, 0]
+    print 'all_psds.shape=',crcns_psds.shape
 
-        xnz = (x > 0) & (r > 0)
-        x = x[xnz]
-        x -= x.mean()
-        x /= x.std(ddof=1)
-
-        r = np.log2(r[xnz])
-        cc = np.corrcoef(x, r)[0, 1]
-
-        plt.plot(x, r, 'go')
-        plt.xlabel('Z-scored Power at %dHz' % f)
-        plt.ylabel('Log Spike Rate')
-        plt.title('cc=%0.2f' % cc)
-        plt.axis('tight')
-    """
-
-    # draw figure
-    fig = plt.figure(figsize=(24, 8))
+    fig = plt.figure(figsize=(24, 5.5))
     fig.subplots_adjust(wspace=0.30, hspace=0.30)
 
     nrows = 1
     ncols = 3
 
-    # plot the grand mean PSD for LFP and PSTH
     ax = plt.subplot(nrows, ncols, 1)
-    plt.plot(pcf.freqs, lfp_psd_mean, '-', c=COLOR_BLUE_LFP, linewidth=4.0, alpha=0.7)
-    plt.plot(pcf.freqs, spike_psd_mean, '-', c=COLOR_YELLOW_SPIKE, linewidth=4.0, alpha=0.7)
-    plt.legend(['LFP', 'Spike PSD'])
+    plt.errorbar(pcf.freqs, lfp_psd_mean, yerr=lfp_psd_std/np.sqrt(nsamps_lfp), c=COLOR_BLUE_LFP, linewidth=4.0, elinewidth=2.0, ecolor='k', alpha=0.7)
+    plt.errorbar(pcf.freqs+3, spike_psd_mean, yerr=spike_psd_std/np.sqrt(nsamps_spike), c=COLOR_YELLOW_SPIKE, linewidth=4.0, elinewidth=2.0, ecolor='k', alpha=0.7)
+    plt.legend(['LFP', 'Spike PSD'], fontsize='x-small')
     plt.axis('tight')
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Power (dB)')
+    plt.ylim(0, 1)
 
-    # plot the grand mean PSD for PSTH using the CRCNS data
     fi = freqs < 200
     ax = plt.subplot(nrows, ncols, 2)
     clrs = ['r', 'g', 'b']
@@ -139,13 +111,16 @@ def plot_psds(psd_file, data_dir='/auto/tdrive/mschachter/data'):
         plt.xlabel('Frequency (Hz)')
         plt.axis('tight')
         # plt.ylim(0, 0.8)
-    plt.legend(['MLd', 'OV', 'Field L+CM'])
+    plt.legend(['MLd', 'OV', 'Field L+CM'], fontsize='x-small', loc='lower right')
 
-    # plot the average spike field coherence
     ax = plt.subplot(nrows, ncols, 3)
-    plt.plot(pcf.freqs, sfc_mean, 'k-', linewidth=4.0, alpha=0.7)
+    fi = pcf.freqs > 20
+    spike_field_coherences_mean[~fi] = 0
+    plt.errorbar(pcf.freqs, spike_field_coherences_mean, yerr=spike_field_coherences_std/np.sqrt(nsamps_spike), c='k', linewidth=4.0,
+                 alpha=0.7, elinewidth=2., ecolor='k')
     plt.xlabel('Frequency (Hz)')
-    plt.ylabel('PSTH-LFP Coherence')
+    plt.ylabel('Coherence')
+    plt.title('PSTH-LFP Coherence')
     plt.axis('tight')
 
     fname = os.path.join(get_this_dir(), 'crcns_data.svg')
