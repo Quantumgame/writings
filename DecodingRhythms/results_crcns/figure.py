83f5173d40ba72c77e61b606dc827400e3a4da74
Mike Schachter <mike.schachter@gmail.com>
2016-01-18 16:52:37 -0800

    updates to crcns figure

--- DecodingRhythms/results_crcns/figure.py
+++ DecodingRhythms/results_crcns/figure.py
@@ -6,6 +6,8 @@ import h5py
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from lasp.colormaps import magma
+from sklearn.covariance import LedoitWolf
 
 from DecodingRhythms.utils import COLOR_YELLOW_SPIKE, get_this_dir, COLOR_BLUE_LFP
 from lasp.sound import log_transform
@@ -21,6 +23,9 @@ def plot_psds(psd_file, data_dir='/auto/tdrive/mschachter/data'):
     pcf = AggregatePairwiseCF.load(pcf_file)
     pcf.zscore_within_site()
 
+    print '# of lfp samples: %d' % (pcf.lfp_psds.shape[0])
+    print '# of spike psd samples: %d' % (pcf.spike_psds.shape[0])
+
     # get the LFP PSDs
     indices = pcf.df['lfp_index'].unique()
     lfp_psds = pcf.lfp_psds[indices, :]
@@ -32,6 +37,16 @@ def plot_psds(psd_file, data_dir='/auto/tdrive/mschachter/data'):
     lfp_psd_std = lfp_psds.std(axis=0, ddof=1)
     nsamps_lfp = lfp_psds.shape[0]
 
+    # zscore the LFP
+    lfp_psds_z = deepcopy(lfp_psds)
+    lfp_psds_z -= lfp_psds.mean(axis=0)
+    lfp_psds_z /= lfp_psds.std(axis=0, ddof=1)
+
+    # compute the LFP PSD covariance
+    # lfp_cov_est = LedoitWolf()
+    # lfp_cov_est.fit(lfp_psds_z)
+    # lfp_cov = lfp_cov_est.covariance_
+
     # get the PSTH PSDs
     indices = pcf.df['spike_index'].unique()
     spike_psds = pcf.spike_psds[indices, :]
@@ -42,6 +57,37 @@ def plot_psds(psd_file, data_dir='/auto/tdrive/mschachter/data'):
     spike_psd_mean = spike_psds.mean(axis=0)
     spike_psd_std = spike_psds.std(axis=0, ddof=1)
     nsamps_spike = spike_psds.shape[0]
+    
+    # zscore the spike psds
+    spike_psds_z = deepcopy(spike_psds)
+    spike_psds_z -= spike_psds.mean(axis=0)
+    spike_psds_z /= spike_psds.std(axis=0, ddof=1)
+
+    # compute the spike PSD covariance
+    # spike_cov_est = LedoitWolf()
+    # spike_cov_est.fit(spike_psds_z)
+    # spike_cov = spike_cov_est.covariance_
+
+    # concatenate the lfp and spike psds
+    nfreqs = len(pcf.freqs)
+    lfp_and_spike_psds = np.zeros([len(pcf.df), nfreqs*2])
+    nz = np.zeros(len(pcf.df), dtype='bool')
+    for k,(lfp_index,spike_index) in enumerate(zip(pcf.df['lfp_index'], pcf.df['spike_index'])):
+        lpsd = pcf.lfp_psds[lfp_index, :]
+        spsd = pcf.spike_psds[spike_index, :]
+        nz[k] = np.abs(lpsd).sum() > 0 and np.abs(spsd).sum() > 0
+        lfp_and_spike_psds[k, :nfreqs] = lpsd
+        lfp_and_spike_psds[k, nfreqs:] = spsd
+
+    # zscore the concatenated matrix
+    lfp_and_spike_psds = lfp_and_spike_psds[nz, :]
+    lfp_and_spike_psds -= lfp_and_spike_psds.mean(axis=0)
+    lfp_and_spike_psds /= lfp_and_spike_psds.std(axis=0, ddof=1)
+
+    # compute the covariance
+    lfp_and_spike_cov_est = LedoitWolf()
+    lfp_and_spike_cov_est.fit(lfp_and_spike_psds)
+    lfp_and_spike_cov = lfp_and_spike_cov_est.covariance_
 
     # get the spike field coherence
     indices = pcf.df['spike_index'].unique()
@@ -73,24 +119,27 @@ def plot_psds(psd_file, data_dir='/auto/tdrive/mschachter/data'):
 
     print 'all_psds.shape=',crcns_psds.shape
 
-    fig = plt.figure(figsize=(24, 5.5))
-    fig.subplots_adjust(wspace=0.30, hspace=0.30)
+    fig = plt.figure(figsize=(24, 12))
+    fig.subplots_adjust(left=0.05, right=0.95, wspace=0.30, hspace=0.30)
 
-    nrows = 1
-    ncols = 3
+    nrows = 2
+    ncols = 100
+    gs = plt.GridSpec(nrows, ncols)
 
-    ax = plt.subplot(nrows, ncols, 1)
-    plt.errorbar(pcf.freqs, lfp_psd_mean, yerr=lfp_psd_std/np.sqrt(nsamps_lfp), c=COLOR_BLUE_LFP, linewidth=4.0, elinewidth=2.0, ecolor='k', alpha=0.7)
-    plt.errorbar(pcf.freqs+3, spike_psd_mean, yerr=spike_psd_std/np.sqrt(nsamps_spike), c=COLOR_YELLOW_SPIKE, linewidth=4.0, elinewidth=2.0, ecolor='k', alpha=0.7)
+    ax = plt.subplot(gs[0, :35])
+    plt.errorbar(pcf.freqs, lfp_psd_mean, yerr=lfp_psd_std/np.sqrt(nsamps_lfp), c=COLOR_BLUE_LFP, linewidth=9.0, elinewidth=2.0, ecolor='k', alpha=0.7)
+    plt.errorbar(pcf.freqs+3, spike_psd_mean, yerr=spike_psd_std/np.sqrt(nsamps_spike), c=COLOR_YELLOW_SPIKE, linewidth=9.0, elinewidth=2.0, ecolor='k', alpha=0.7)
     plt.legend(['LFP', 'Spike PSD'], fontsize='x-small')
     plt.axis('tight')
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Power (dB)')
     plt.ylim(0, 1)
+    plt.title('Mean LFP and PSTH PSDs')
 
     fi = freqs < 200
-    ax = plt.subplot(nrows, ncols, 2)
-    clrs = ['r', 'g', 'b']
+    ax = plt.subplot(gs[1, :35])
+    clrs = ['k', '#d60036', COLOR_YELLOW_SPIKE]
+    alphas = [0.8, 0.8, 0.6]
     for k,reg in enumerate(['brainstem', 'thalamus', 'cortex']):
 
         i = cell_df.superregion == reg
@@ -105,23 +154,26 @@ def plot_psds(psd_file, data_dir='/auto/tdrive/mschachter/data'):
         psd_cv = psd_std / psd_mean
 
         # plot the mean power spectrum on the left
-
-        plt.plot(freqs[fi], psd_mean[fi], c=clrs[k], linewidth=4.0, alpha=0.7)
+        plt.plot(freqs[fi], psd_mean[fi], c=clrs[k], linewidth=9.0, alpha=alphas[k])
         plt.ylabel('Power (dB)')
         plt.xlabel('Frequency (Hz)')
         plt.axis('tight')
-        # plt.ylim(0, 0.8)
-    plt.legend(['MLd', 'OV', 'Field L+CM'], fontsize='x-small', loc='lower right')
+        plt.ylim(0, 1.0)
+    plt.legend(['MLd', 'OV', 'Field L+CM'], fontsize='x-small', loc='upper right')
+    plt.title('Mean PSTH PSDs (CRCNS Data)')
 
-    ax = plt.subplot(nrows, ncols, 3)
-    fi = pcf.freqs > 20
-    spike_field_coherences_mean[~fi] = 0
-    plt.errorbar(pcf.freqs, spike_field_coherences_mean, yerr=spike_field_coherences_std/np.sqrt(nsamps_spike), c='k', linewidth=4.0,
-                 alpha=0.7, elinewidth=2., ecolor='k')
+    ax = plt.subplot(gs[:, 40:])
+    plt.imshow(lfp_and_spike_cov, aspect='auto', interpolation='nearest', origin='lower', cmap=magma, vmin=0, vmax=1)
+    plt.colorbar()
+    xy = np.arange(2*len(pcf.freqs))
+    lbls = np.concatenate([['%d' % f for f in pcf.freqs], ['%d' % f for f in pcf.freqs]])
+    plt.xticks(xy, lbls, rotation=45)
+    plt.yticks(xy, lbls)
+    plt.axhline(nfreqs-0.5, c='w')
+    plt.axvline(nfreqs-0.5, c='w')
     plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Coherence')
-    plt.title('PSTH-LFP Coherence')
-    plt.axis('tight')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('LFP-PSTH Correlation Matrix')
 
     fname = os.path.join(get_this_dir(), 'crcns_data.svg')
     plt.savefig(fname, facecolor='w', edgecolor='none')
