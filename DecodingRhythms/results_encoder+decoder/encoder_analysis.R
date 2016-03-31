ed1cf3c5366616bbda29776bd871150db8b441a6
mschachter <mike.schachter@gmail.com>
2016-03-31 16:22:07 -0700

    new figure for pairwise encoder perfs/weights

--- DecodingRhythms/results_encoder+decoder/encoder_analysis.R
+++ DecodingRhythms/results_encoder+decoder/encoder_analysis.R
@@ -2,6 +2,10 @@ library(car)
 library(nlme)
 library(effects)
 
+#######################################
+# PSD Encoder Analysis
+#######################################
+
 d = read.csv('/auto/tdrive/mschachter/data/aggregate/encoder_perfs_for_glm.csv')
 d$freq = as.factor(d$freq)
 
@@ -24,3 +28,24 @@ Anova(m)
 summary(m)
 
 # m = lm(w ~ aprop*freq*region, data=d)  # triplet interactions are not very significant
+
+
+#####################################
+# Pairwise Encoder Analysis
+#####################################
+
+d = read.csv('/auto/tdrive/mschachter/data/aggregate/pairwise_encoder_perfs_for_glm.csv')
+d$lag = as.factor(d$lag)
+d = subset(d, d$r2 > 0)
+
+m = lm(r2 ~ regions + lag, data=d)
+summary(m)
+Anova(m)
+
+
+d = read.csv('/auto/tdrive/mschachter/data/aggregate/pairwise_encoder_weights_for_glm.csv')
+d$lag = as.factor(d$lag)
+i = abs(d$w) > 3e-1
+d = subset(d, i)
+m = lm(w ~ aprop:lag, data=d)
+summary(m)
