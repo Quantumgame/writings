da44eae9f2c998e83d69957af271ebc7ca862d07
Mike Schachter <mike.schachter@gmail.com>
2016-06-13 10:16:10 -0700

    updated ensemble figure to include spikes

--- DecodingRhythms/results_ensemble_decoder/figure.py
+++ DecodingRhythms/results_ensemble_decoder/figure.py
@@ -12,16 +12,16 @@ from zeebeez.utils import ACOUSTIC_PROP_NAMES
 
 def draw_figures(agg, data_dir='/auto/tdrive/mschachter/data'):
 
-    i = agg.df.decomp == 'full_psds'
+    decomps = ['spike_rate', 'full_psds']
 
     aprops = ['entropytime', 'sal', 'meanspect', 'maxAmp', 'stdtime']
     curves_by_prop = dict()
     for aprop in aprops:
-        curves_by_prop[aprop] = list()
+        for decomp in decomps:
+            curves_by_prop[(decomp, aprop)] = list()
 
-    df = agg.df[i]
-    g = df.groupby(['bird', 'block', 'segment', 'aprop'])
-    for (bird,block,segment,aprop),gdf in g:
+    g = agg.df.groupby(['bird', 'block', 'segment', 'aprop', 'decomp'])
+    for (bird,block,segment,aprop,decomp),gdf in g:
 
         lst = zip(gdf.num_units.values, gdf.r2_mean.values, gdf.r2_std.values)
         lst.sort(key=operator.itemgetter(0))
@@ -30,7 +30,7 @@ def draw_figures(agg, data_dir='/auto/tdrive/mschachter/data'):
         r2 = [x[1] for x in lst]
         r2_std = [x[2] for x in lst]
 
-        curves_by_prop[aprop].append((nu, r2, r2_std))
+        curves_by_prop[(decomp, aprop)].append((nu, r2, r2_std))
 
     figsize = (23, 13)
     fig = plt.figure(figsize=figsize)
@@ -40,14 +40,22 @@ def draw_figures(agg, data_dir='/auto/tdrive/mschachter/data'):
     nrows = 3
     gs = plt.GridSpec(nrows, ncols)
 
-    for k,aprop in enumerate(aprops):
-        ax = plt.subplot(gs[0, k])
-        for nu,r2,r2_std in curves_by_prop[aprop]:
-            plt.plot(nu, r2, 'k-', linewidth=3.0, alpha=0.7)
-        plt.title(ACOUSTIC_PROP_NAMES[aprop])
-        plt.xlabel('# of Electrodes')
-        plt.ylabel('Decoder R2')
-        plt.axis('tight')
+    for j,decomp in enumerate(decomps):
+        for k,aprop in enumerate(aprops):
+            ax = plt.subplot(gs[j, k])
+            for nu,r2,r2_std in curves_by_prop[(decomp,aprop)]:
+                plt.plot(nu, r2, 'k-', linewidth=3.0, alpha=0.7)
+            plt.title(ACOUSTIC_PROP_NAMES[aprop])
+            plt.axis('tight')
+
+            if decomp == 'full_psds':
+                plt.xlabel('# of Electrodes')
+                plt.ylabel('Decoder R2 (LFP PSD)')
+            else:
+                plt.xlabel('# of Neurons')
+                plt.ylabel('Decoder R2 (Spike Rate)')
+                plt.xlim(1, 64)
+
 
     fname = os.path.join(get_this_dir(), 'figure.svg')
     plt.savefig(fname, facecolor='w', edgecolor='none')
