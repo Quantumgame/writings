c05af56df04aabadd441887186c114caaacd6786
mschachter <mike.schachter@gmail.com>
2016-07-25 18:34:40 -0700

    started on thesis...

---
+++ thesis/doit.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+latex thesis && \
+biber thesis && \
+latex thesis && \
+pdflatex thesis.tex
+
+# with biblatex, it is only necessary to run latex once after bibtex, not twice.
+# with older tex installations, you may need to change 'biber' to 'bibtex'
